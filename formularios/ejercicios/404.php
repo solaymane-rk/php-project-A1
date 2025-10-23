ee5f655c12df6b8bc52f2d7d21b9dd89e8d7981d
Solaymane Roukdi <sroukdiamhaj@gmail.com>
2025-10-23 12:56:37 +0200

    Restauración ej formulario

--- formularios/ejercicios/404.php
+++ formularios/ejercicios/404.php
@@ -1,5 +1,27 @@
 <?php
-$page_title = "Ejercicio formularios";
-$current_page = "formularios";
-require_once '../includes/header.php';
+$current_page = 'ej';
+
+require_once '../../includes/navbar.php';
+
 ?>
+<form action="404-subida.php" method="post">
+    <legend>Subida de fichero</legend>
+
+    <label for="archivo">Archivo</label>
+    <input type="file" name="archivo" require>
+
+    <br><br>
+
+    <label for="anchura">Anchura</label>
+    <input type="number" name="anchura" required>
+
+    <br><br>
+
+    <label for="altura">Altura</label>
+    <input type="number" name="altura" required>
+
+    <br><br>
+
+    <button type="submit">Enviar</button>
+
+</form>
--- formularios/ejercicios/405.php
+++ formularios/ejercicios/405.php
@@ -1,5 +1,26 @@
 <?php
-$page_title = "Ejercicio formularios";
-$current_page = "formularios";
-require_once '../includes/header.php';
+$current_page = 'ej';
+
+require_once '../../includes/navbar.php';
+
 ?>
+<form action="405-img.php" method="post" enctype="multipart/form-data">
+    <legend>Subida de fichero</legend>
+
+    <label for="archivo">Archivo</label>
+    <input type="file" name="archivo" required>
+
+    <br><br>
+
+    <label for="anchura">Anchura</label>
+    <input type="number" name="anchura" required>
+
+    <br><br>
+
+    <label for="altura">Altura</label>
+    <input type="number" name="altura" required>
+
+    <br><br>
+
+    <button type="submit">Enviar</button>
+</form>
